62129aa7c1885d83cc75e59ca8d01b4e777995fc
capivara00700 <155832965+capivara00700@users.noreply.github.com>
2024-02-27 02:29:39 -0300

    Update js.js

---
+++ js.js
@@ -0,0 +1,49 @@
+const pokemonName = document.querySelector('.pkm_nome');
+const pokemonNumber = document.querySelector('.pkm_numero');
+const pokemonFoto = document.querySelector('.pkm');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn_prev');
+const buttonNext = document.querySelector('.btn_next');
+
+const Buscarpkm = async (pokemon) => {
+const Resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    
+if(Resposta.status === 200){
+    const data = await Resposta.json();
+    return data;
+}
+
+}
+
+
+const renderpokemon = async (pokemon) => {
+
+    pokemonName.innerHTML = 'Loading...'
+    pokemonNumber.innerHTML = ''
+
+    const data = await Buscarpkm(pokemon)
+
+    if(data){
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id
+        pokemonFoto.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    } else {
+        pokemonName.innerHTML = 'Not found'
+        pokemonNumber.innerHTML = '0'
+        pokemonFoto.src = "https://i.pinimg.com/originals/0b/61/1f/0b611f10b6bf172d2b70d94cc515b6d5.png"
+    }
+}
+
+
+
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault()
+
+    renderpokemon(input.value.toLowerCase())
+    input.value = ''
+})
+
+renderpokemon('1')
